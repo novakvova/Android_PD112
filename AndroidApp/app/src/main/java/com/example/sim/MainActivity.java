99e5fb68a6e3453342cf28a0b3f78e06a596483c
novakvova <novakvova@gmail.com>
2024-04-08 17:38:56 +0300

    Begin working view image url

--- AndroidApp/app/src/main/java/com/example/sim/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/sim/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView ivAvatar = findViewById(R.id.ivAvatar);
+        String url = "https://content1.rozetka.com.ua/goods/images/big/343033787.jpg";
+        Glide.with(this)
+                .load(url)
+                .apply(new RequestOptions().override(400))
+                .into(ivAvatar);
+
     }
 }
