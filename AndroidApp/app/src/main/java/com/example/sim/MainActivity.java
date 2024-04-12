7f262f329b7efa6f2c802fd3170d7412c9a81b0f
novakvova <novakvova@gmail.com>
2024-04-12 17:00:21 +0300

    Begin working retrofit

--- AndroidApp/app/src/main/java/com/example/sim/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/sim/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.sim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.sim.dto.category.CategoryItemDTO;
+import com.example.sim.services.ApplicationNetwork;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
                 .load(url)
                 .apply(new RequestOptions().override(400))
                 .into(ivAvatar);
+
+        ApplicationNetwork
+                .getInstance()
+                .getCategoriesApi()
+                .list()
+                .enqueue(new Callback<List<CategoryItemDTO>>() {
+                    @Override
+                    public void onResponse(Call<List<CategoryItemDTO>> call, Response<List<CategoryItemDTO>> response) {
+                        List<CategoryItemDTO> items = response.body();
+                        Log.d("--list categories--", String.valueOf(items.size()));
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<CategoryItemDTO>> call, Throwable throwable) {
+
+                    }
+                });
 
     }
 }
---
+++ AndroidApp/app/src/main/java/com/example/sim/contants/Urls.java
@@ -0,0 +1,5 @@
+package com.example.sim.contants;
+
+public class Urls {
+    public static final String BASE="https://pd112.itstep.click";
+}
