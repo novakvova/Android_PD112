d496c8f5f4ccb227960639752067d2bc751d7b6e
novakvova <novakvova@gmail.com>
2024-04-15 15:29:18 +0300

    Show list cards

--- AndroidApp/app/src/main/java/com/example/sim/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/sim/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sim;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +10,7 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.sim.category.CategoriesAdapter;
 import com.example.sim.dto.category.CategoryItemDTO;
 import com.example.sim.services.ApplicationNetwork;
 
@@ -18,19 +21,24 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rcCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView ivAvatar = findViewById(R.id.ivAvatar);
+//        ImageView ivAvatar = findViewById(R.id.ivAvatar);
 //        String url = "https://content1.rozetka.com.ua/goods/images/big/343033787.jpg";
 //        String url = "http://10.0.2.2:5101/images/1.jpg";
-        String url = "https://pd112.itstep.click/images/1.jpg";
-        Glide.with(this)
-                .load(url)
-                .apply(new RequestOptions().override(400))
-                .into(ivAvatar);
+//        String url = "https://pd112.itstep.click/images/1.jpg";
+//        Glide.with(this)
+//                .load(url)
+//                .apply(new RequestOptions().override(400))
+//                .into(ivAvatar);
+
+        rcCategories = findViewById(R.id.rcCategories);
+        rcCategories.setHasFixedSize(true);
+        rcCategories.setLayoutManager(new GridLayoutManager(this, 1, RecyclerView.VERTICAL, false));
 
         ApplicationNetwork
                 .getInstance()
@@ -40,7 +48,9 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(Call<List<CategoryItemDTO>> call, Response<List<CategoryItemDTO>> response) {
                         List<CategoryItemDTO> items = response.body();
-                        Log.d("--list categories--", String.valueOf(items.size()));
+                        CategoriesAdapter ca = new CategoriesAdapter(items);
+                        rcCategories.setAdapter(ca);
+                        //Log.d("--list categories--", String.valueOf(items.size()));
                     }
 
                     @Override
