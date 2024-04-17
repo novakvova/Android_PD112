58294eac7fba9a277630f215c878b0b52468716f
novakvova <novakvova@gmail.com>
2024-04-17 15:44:25 +0300

    Working add image category

--- ShopApp/ShopApp/Data/Entities/CategoryEntity.cs
+++ ShopApp/ShopApp/Data/Entities/CategoryEntity.cs
@@ -15,5 +15,8 @@ namespace ShopApp.Data.Entities
         [StringLength(4000)]
         public string? Description { get; set; }
 
+        [StringLength(255)]
+        public string? Image { get; set; }
+
     }
 }
--- ShopApp/ShopApp/Models/Categories/CategoryCreateViewModel.cs
+++ ShopApp/ShopApp/Models/Categories/CategoryCreateViewModel.cs
@@ -4,5 +4,6 @@
     {
         public required string Name { get; set; }
         public string? Description { get; set; }
+        public IFormFile? Image { get; set; }
     }
 }
