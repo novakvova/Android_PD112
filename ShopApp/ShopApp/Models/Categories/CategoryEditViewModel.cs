2f19846f0a66e164b04ba6df69eb681086cf2c0a
novakvova <novakvova@gmail.com>
2024-04-20 09:25:54 +0300

    Is Good Edit and Delete image

--- ShopApp/ShopApp/Models/Categories/CategoryEditViewModel.cs
+++ ShopApp/ShopApp/Models/Categories/CategoryEditViewModel.cs
@@ -5,5 +5,6 @@
         public required int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        public IFormFile? Image { get; set; }
     }
 }
