0a09a06bd976bdf7d6fff46691d2339fbb11c79a
novakvova <novakvova@gmail.com>
2024-04-22 15:19:20 +0300

    Working context Identity

---
+++ ShopApp/ShopApp/Data/Entities/Identity/UserRoleEntity.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ShopApp.Data.Entities.Identity
+{
+    public class UserRoleEntity : IdentityUserRole<long>
+    {
+        public virtual UserEntity? User { get; set; }
+        public virtual RoleEntity? Role { get; set; }
+    }
+}
--- ShopApp/ShopApp/Data/MyAppContext.cs
+++ ShopApp/ShopApp/Data/MyAppContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ShopApp.Data.Entities;
+using ShopApp.Data.Entities.Identity;
 
 namespace ShopApp.Data
 {
-    public class MyAppContext : DbContext
+    public class MyAppContext : IdentityDbContext<UserEntity, RoleEntity, long,
+        IdentityUserClaim<long>, UserRoleEntity, IdentityUserLogin<long>,
+        IdentityRoleClaim<long>, IdentityUserToken<long>>
     {
         public MyAppContext(DbContextOptions<MyAppContext> options) 
             : base(options)
